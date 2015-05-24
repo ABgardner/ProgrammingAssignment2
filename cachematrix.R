4116192c23b86dcf8c98386611acce4cc12dcded
Ahren Baumgardner <ahrenbaumgardner@gmail.com>
2015-05-24 10:48:58 +0300

    First Commit Error, Second Attempt

--- cachematrix.R
+++ cachematrix.R
@@ -5,7 +5,17 @@
 ## retrive a square matrix and it's inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function()x
+  setinverse <- function(solve) m <<- solve
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
